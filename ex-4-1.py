4010c0a54a859459ad7cca458caab331e5e512c1
MarsH-coder <marshuang26@gmail.com>
2024-08-06 20:11:29 +0800

    Create ex-4-1.py

---
+++ ex-4-1.py
@@ -0,0 +1,14 @@
+b = "Hello, World!"
+print(b[:5])
+
+b = "Hello, World!"
+print(b[2:5])
+
+b = "Hello, World!"
+print(b[2:])
+
+b = "Hello, World!"
+print(b[-5:-2])
+)
+THG= "The Hunger Games"
+print(THG[4:]
